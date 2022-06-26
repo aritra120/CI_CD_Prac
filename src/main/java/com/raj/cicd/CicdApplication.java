cfaed8f91df213c4956901a71bf938ea424a8e6a
aritra120 <aritradutta120@gmail.com>
2022-06-26 23:43:41 +0530

    I have added pom.xml , /src , .gitignore files

---
+++ src/main/java/com/raj/cicd/CicdApplication.java
@@ -0,0 +1,21 @@
+package com.raj.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdApplication {
+	
+	@GetMapping("/home")
+	public String getMessage() {
+		return "Welcome to Spring Boot";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+
+}
